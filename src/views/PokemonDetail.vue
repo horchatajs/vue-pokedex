5d48dacf667cc18d4c8672a5240c0af760a01026
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2019-05-15 23:49:46 -0600

    Update template

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="container mx-auto">
     <div class="px-4">
-      <h1 class="capitalize mb-2">Bulbasaur</h1>
+      <h1 class="capitalize mb-2">{{pokemon.name}}</h1>
       <div class="flex mb-12">
         <!-- types -->
       </div>
       <div class="w-full flex flex-col md:flex-row" v-if="Object.keys(pokemon).length">
         <div class="w-1/2 text-lg">
           <p class="mb-8">
-            <span class="font-bold">Experiencia:</span> 50
+            <span class="font-bold">Experiencia:</span>
+            <!-- Agregar experiencia -->
           </p>
           <p class="mb-8">
-            <span class="font-bold">Peso:</span> 100
+            <span class="font-bold">Peso:</span>
+            <!-- Agregar peso -->
           </p>
           <p class="mb-8">
-            <span class="font-bold">Altura:</span> 150
+            <span class="font-bold">Altura:</span>
+            <!-- Agregar altura -->
           </p>
           <p class="mb-8">
-            <span class="font-bold">Habilidades:</span> First, second
+            <span class="font-bold">Habilidades:</span>
+            <span>{{ abilities }}</span>
           </p>
         </div>
-      <pokemon :pokemon="pokemon"></pokemon>
+        <pokemon :pokemon="pokemon"></pokemon>
       </div>
       <div class="text-center">
         <router-link :to="{name: 'home'}" class="no-underline font-bold text-lg">Regresar</router-link>
