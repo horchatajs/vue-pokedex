16b1be8c972f2b5ffaf6ca4e67363ca2ecaa15fc
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2019-05-15 23:48:39 -0600

    Remove features

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
-import './registerServiceWorker';
 import '@/assets/app.css';
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = true;
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
