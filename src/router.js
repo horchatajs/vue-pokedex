316bf1b4e9be67095c04e5b82c3de5947ecea06c
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2019-05-15 23:48:58 -0600

    Add new NotFound route

--- src/router.js
+++ src/router.js
@@ -1,21 +1,32 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
-import Pokemon from "./views/PokemonDetail.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Pokemon from './views/PokemonDetail.vue';
+import NotFound from './views/NotFound.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'home',
+      component: Home,
     },
     {
-      path: "/pokemon/:name",
-      name: "pokemon",
-      component: Pokemon
-    }
-  ]
+      path: '/pokemon/:name',
+      name: 'pokemon',
+      component: Pokemon,
+    },
+    {
+      path: '/pokemon/**',
+      name: 'pokemon-not-found',
+      component: NotFound,
+    },
+    {
+      path: '*',
+      name: 'not-found',
+      component: NotFound,
+    },
+  ],
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mx-auto">
+    <div class="px-4">
+      <h1 class="capitalize mb-2">404</h1>
+    </div>
+  </div>
+</template>
